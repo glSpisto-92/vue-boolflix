316f9d325f46d6c70d2bf760c4439c3b501ecd25
Gianluigi Spisto <luigi.spisto92@gmail.com>
2021-04-09 15:04:49 +0200

    Aggiunta struttura html, css(da ultimare) e js

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let app = new Vue (
+  {
+    el: '#app',
+    data: {
+      nameMovie: '',
+      movies: [],
+
+    },
+    methods: {
+
+      searchMovie: function(){
+
+        axios.get('https://api.themoviedb.org/3/search/movie?',{
+
+          params: {
+            api_key: 'af468508a333025a615170246e061ead',
+            query: this.nameMovie,
+            language: 'it-IT',
+          }
+
+        })
+        .then((response) => {
+        this.movies = response.data.results;
+        })
+      }
+    }
+  }
+);
